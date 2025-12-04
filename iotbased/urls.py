26003d21de174f6f5281046bc94e1bb2efea31f5
amnamalik4050 <malik5157pp@gmail.com>
2025-12-04 18:49:05 +0500

    Delete iotbased directory
    delete message

--- iotbased/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-
-    # ✅ iot_app ke URLs load karo
-    path('', include('iot_app.urls')),
-]
